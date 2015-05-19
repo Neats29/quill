d861e9ffd7361c14ce5e3530800e10b651800ac2
Anita Amini <baadge29@yahoo.co.uk>
2015-05-19 11:24:54 +0100

    test stuff

--- test/__qunit.js
+++ test/__qunit.js
@@ -1,4 +1,14 @@
-QUnit.test( "hello test", function( assert ) {
+test("hello test", function(assert) {
 	"use strict";
-  assert.ok( 1 == "1", "Passed!" );
+  assert.ok(1 == "1", "Passed!" );
+});
+
+test("is there a canvas element", function(assert) {
+	"use strict";
+  assert.ok();
+});
+
+test("is there a anything inside the canvas element", function(assert) {
+	"use strict";
+  assert.ok();
 });
