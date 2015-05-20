8934203b3074a593b0fdfaa82a0065323bd185a8
Anita Amini <baadge29@yahoo.co.uk>
2015-05-20 17:07:35 +0100

    undo, flipbook, colors etc

--- app/js/app.js
+++ app/js/app.js
@@ -42,30 +42,77 @@ var state = {
   history: []
 };
 
-addClickHandler(document.getElementById("canvas"), function(e) {
+addClickHandler(document.getElementById("canvas"), function (e) {
   var cell = editor.getClickedCell(e);
-  state.canvas[cell[1]][cell[0]] = state.color;
   state.history.push(JSON.parse(JSON.stringify(state.canvas)));
+
+  state.canvas[cell[1]][cell[0]] = state.color;
 
   render(state);
 });
 
-addClickHandler(document.getElementById("pick-color"), function(e) {
+addClickHandler(document.getElementById("pick-color"), function () {
   var color = "#" + document.getElementById("color-value").value;
   var validHex  = /^#[0-9A-F]{6}$/i.test(color);
 
   if (validHex) {
     state.color = color;
-    render(state);
   }
 });
 
-addClickHandler(document.getElementById("play"), function(e) {
-	state.history.forEach(function(e) {
-		setTimeout(function() {
-			flipbook.draw(e, state.width, state.height);
-		}, 400);
-	});
+addClickHandler(document.getElementById("rubber"), function() {
+	state.color = "#FFFFFF";
 });
+
+
+addClickHandler(document.getElementById("e71b1b"), function() {
+	state.color = "#e71b1b";
+
+});
+
+addClickHandler(document.getElementById("3639E0"), function() {
+	state.color = "#3639E0";
+
+});
+
+addClickHandler(document.getElementById("89e71b"), function() {
+	state.color = "#89e71b";
+
+});
+
+addClickHandler(document.getElementById("000000"), function() {
+	state.color = "#000000";
+
+});
+
+addClickHandler(document.getElementById("thick"), function() {
+	editor.colorCells();
+
+});
+
+addClickHandler(document.getElementById("play"), function () {
+
+	var historyCounter = state.history.length - 1;
+
+	function replayDrawing(n) {
+		if(n>historyCounter) return;
+		flipbook.draw(state.history[n]);
+
+		setTimeout(function(){
+			replayDrawing(n+1);
+		}, 400)
+	}
+
+	replayDrawing(0);
+
+});
+
+addClickHandler(document.getElementById("undo"), function () {
+	if (state.history.length < 1) return;
+
+	var lastCanvas = state.history.pop();
+	state.canvas = lastCanvas;
+	render(state);
+})
 
 render(state);
